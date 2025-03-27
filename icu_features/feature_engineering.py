e7c8f0b5ccb607c41a30da09cc97fc92d8f281b6
Malte Londschien <malte@londschien.de>
2025-03-27 17:37:52 +0100

    Remove crea_baseline from kdigo_3 args

--- icu_features/feature_engineering.py
+++ icu_features/feature_engineering.py
@@ -585,7 +585,7 @@ def outcomes():
     # kidney_failure_at_48h
     # The patient has a kidney failure if they are in stage 3 according to
     # https://kdigo.org/wp-content/uploads/2016/10/KDIGO-2012-AKI-Guideline-English.pdf
-    def kdigo_3(crea, crea_baseline, rel_urine_rate, crrt):
+    def kdigo_3(crea, rel_urine_rate, crrt):
         """Compute the KDIGO stage 3 kidney failure label."""
         crea_baseline = crea.shift(1).rolling_min(7 * 24, min_samples=1)
 
